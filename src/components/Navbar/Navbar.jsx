3c2462719be75a66d94e159fa2502ac495141d01
dTap <imtapped@protonmail.com>
2022-03-13 22:09:30 +0700

    added framer motion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { images } from '../../constants';
+import React, { useState } from 'react';
 
+// Import menu icons
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
+
+import { images } from '../../constants';
 import './Navbar.scss';
 
 
-
-
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -20,8 +25,32 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      {/* The mobile app hamburger menu */}
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        
+        {/* This is how to set framer-motion */}
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar
