72d794977eaeaf8fd7a05113622cd810394ae912
dTap <imtapped@protonmail.com>
2022-03-16 01:15:33 +0700

    added social media icons and side slider

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 // To export Navbar component to App.js
 export { default as Navbar } from './Navbar/Navbar';
+export { default as NavigationDots } from './NavigationDots';
+export { default as SocialMedia } from './SocialMedia';
