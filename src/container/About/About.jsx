12ac9c851a1fa596c25751e2e63818e12754399b
dTap <imtapped@protonmail.com>
2022-03-15 21:54:35 +0700

    added new logo and changed font

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -15,7 +15,7 @@ const abouts = [
 const About = () => {
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+      <h2 className="head-text"><span>Great Communication</span> <br />means  <span>Successful Business!</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
